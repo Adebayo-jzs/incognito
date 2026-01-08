1f97fd29a373635ade7d4df5a7e0aca7e9ac43b7
Adebayo-jzs <adedejiadebayo732@gmail.com>
2026-01-08 22:19:55 +0100

    landing page and ui improvement,profile,messages,login and signup page

--- app/layout.js
+++ app/layout.js
@@ -20,7 +20,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased  bg-gradient-to-tr from-[#15121a]  via-[#141118] to-[#1c1425] `}
       >
         {children}
       </body>
